d6bc7c9e951ed48c71c4caf2f5a930826974d695
vivek negi <vnegi@vmware.com>
2021-05-13 07:44:05 +0530

    first commit

--- src/comic/fatheridontwanttogetmarried/data.js
+++ src/comic/fatheridontwanttogetmarried/data.js
@@ -4,12 +4,6 @@ const data=[
     },
     {
         imgUrl:"https://yeah811651282.files.wordpress.com/2021/05/0009.jpeg"
-    },
-    {
-        imgUrl:"/0113.jpeg"
-    },
-    {
-        imgUrl:"/0111.jpeg"
     }
 ]
 
