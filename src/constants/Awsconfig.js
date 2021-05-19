37dfdce5a6738b72a6e2c2c58bca6115860edf4b
vivek negi <vnegi@vmware.com>
2021-05-19 10:08:08 +0530

    removing aws file

--- src/constants/Awsconfig.js
+++ src/constants/Awsconfig.js
@@ -1,6 +1,6 @@
 
 export const aws = {
-    accessKeyId: "AKIAX2JRPSMT3WAVYB5B",
-    secretAccessKey: "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",
+    accessKeyId: "",
+    secretAccessKey: "",
     bucket: "manhuapointin",
 };
